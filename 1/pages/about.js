2036c377d304e4839fe6671658d14a2fefcddee5
gela samsonidze <samosnidze7@yahoo.com>
2022-10-09 01:24:17 +0400

    .

---
+++ 1/pages/about.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const about = () => {
+  return (
+    <div>
+      <h1>This is about page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default about;
